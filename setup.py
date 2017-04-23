dd8380caa90354523a15c5768d74671edb1fe956
Theo Linnemann <Theo.Linnemann@gmail.com>
2017-04-23 15:50:37 -0500

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='iForgetALot',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/ZeroCool2u/iForgetALot',
+    license='WhateverTheFuckYouWant.legal',
+    author='ZeroCool2u & Zthroo',
+    author_email='theo-linnemann@uiowa.edu',
+    description='A sort of password manager thing. '
+)
